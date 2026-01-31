2533173a335e3b54180c2f13fadef8ab8afc4815
kryvenkovladyslav <onekryvenko.vladyslav@gmail.com>
2026-01-31 08:22:28 -0800

    Added secret endpoint

---
+++ authentication/GettingUp.WebApi/Controllers/EndpointsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GettingUp.WebApi.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public sealed class EndpointsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("secret")]
+        public ActionResult<string> Secret()
+        {
+            return this.Ok("Secret Endpoint");
+        }
+    }
+}
